a638ef0a434b430acd285666aafb6bd870f7d76b
dyutiB2907 <dyutisnata2907@gmail.com>
2026-02-17 19:36:11 +0530

    Create UC1.java

---
+++ UC1.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * 
+ * Use Case 1 (UC1): Print literal text "OOPS" to console.
+ * 
+ * This is the foundational step for the OOPS Banner Application.
+ * 
+ * @author Dyutisnata Goswami
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Printing literal text to console
+        System.out.println("OOPS");
+    }
+}
