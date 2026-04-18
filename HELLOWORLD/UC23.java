5764043022a66c617e50b312f13ae859f8a25fba
dyutiB2907 <dyutisnata2907@gmail.com>
2026-04-18 18:02:50 +0530

    Create UC23.java

---
+++ HELLOWORLD/UC23.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "World";   // default value
+
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
