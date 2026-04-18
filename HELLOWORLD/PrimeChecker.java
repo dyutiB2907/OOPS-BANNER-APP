d9c1bf3a54d001e15be106a49d4744c5af1825f4
dyutiB2907 <dyutisnata2907@gmail.com>
2026-04-18 18:18:57 +0530

    Create PrimeChecker.java

---
+++ HELLOWORLD/PrimeChecker.java
@@ -0,0 +1,24 @@
+public class PrimeChecker {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int inputNumber = 29;
+
+        boolean result = isPrime(inputNumber);
+
+        System.out.println("Is Prime: " + result);
+    }
+}
